c03252a80e0ff459689f0eb462bd2e4518a406d5
warhammer327 <rizwansm@outlook.com>
2023-04-18 13:03:38 +0600

    minor fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.2"
+lock "~> 3.16.0"
 
 set :application, "rails-7-app"
 set :repo_url, "git@github.com:warhammer327/rails-7-app.git"
