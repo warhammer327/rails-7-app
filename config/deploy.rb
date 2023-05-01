a99362e7476092e66f2fa1138bc0324a10f11966
warhammer327 <rizwansm@outlook.com>
2023-05-01 17:26:34 +0600

    ..

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,15 +30,52 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
 namespace :puma do
-desc 'Create Directories for Puma Pids and Socket'
-task :make_dirs do
-on roles(:app) do
-execute "mkdir #{shared_path}/tmp/sockets -p"
-execute "mkdir #{shared_path}/tmp/pids -p"
+    desc 'Create Directories for Puma Pids and Socket'
+    task :make_dirs do
+        on roles(:app) do
+            execute "mkdir #{shared_path}/tmp/sockets -p"
+            execute "mkdir #{shared_path}/tmp/pids -p"
+        end
+    end
+    before :start, :make_dirs
 end
-end
-before :start, :make_dirs
+
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+
+      # Update this to your branch name: master, main, etc. Here it's main
+      unless `git rev-parse HEAD` == `git rev-parse origin/main`
+        puts "WARNING: HEAD is not the same as origin/main"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+    task :restart do
+      on roles(:app), in: :sequence, wait: 5 do
+        invoke 'puma:restart'
+      end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  # after  :finishing,    :restart
 end
 
 
